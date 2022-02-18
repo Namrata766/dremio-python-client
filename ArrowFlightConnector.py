c5fee3190b3057ed0b28f9bb06892660ac3d3e72
Namrata Kumari <namrata_kumari@dell.com>
2022-02-18 10:59:59 +0530

    First commit

---
+++ ArrowFlightConnector.py
@@ -0,0 +1,172 @@
+"""
+  Copyright (C) 2017-2021 Dremio Corporation
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+import argparse
+import sys
+
+from pyarrow import flight
+
+
+class DremioClientAuthMiddlewareFactory(flight.ClientMiddlewareFactory):
+    """A factory that creates DremioClientAuthMiddleware(s)."""
+
+    def __init__(self):
+        self.call_credential = []
+
+    def start_call(self, info):
+        return DremioClientAuthMiddleware(self)
+
+    def set_call_credential(self, call_credential):
+        self.call_credential = call_credential
+
+
+class DremioClientAuthMiddleware(flight.ClientMiddleware):
+    """
+    A ClientMiddleware that extracts the bearer token from 
+    the authorization header returned by the Dremio 
+    Flight Server Endpoint.
+
+    Parameters
+    ----------
+    factory : ClientHeaderAuthMiddlewareFactory
+        The factory to set call credentials if an
+        authorization header with bearer token is
+        returned by the Dremio server.
+    """
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def received_headers(self, headers):
+        auth_header_key = 'authorization'
+        authorization_header = []
+        for key in headers:
+          if key.lower() == auth_header_key:
+            authorization_header = headers.get(auth_header_key)
+        self.factory.set_call_credential([
+            b'authorization', authorization_header[0].encode("utf-8")])
+
+
+def parse_arguments():
+    """
+    Parses the command-line arguments supplied to the script.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', '--hostname', type=str, help='Dremio co-ordinator hostname',
+      default='xyz.com')
+    parser.add_argument('-port', '--flightport', type=str, help='Dremio flight server port',
+      default='32010')
+    parser.add_argument('-user', '--username', type=str, help='Dremio username',
+      required=True)
+    parser.add_argument('-pass', '--password', type=str, help='Dremio password',
+      required=True)
+    parser.add_argument('-query', '--sqlquery', type=str, help='SQL query to test',
+      required=False)
+    parser.add_argument('-tls', '--tls', dest='tls', help='Enable encrypted connection',
+      required=False, default=False, action='store_true')
+    parser.add_argument('-disableServerVerification', '--disableServerVerification', dest='disableServerVerification',
+                        help='Disable TLS server verification',
+                        required=False, default=False)
+    parser.add_argument('-certs', '--trustedCertificates', type=str,
+      help='Path to trusted certificates for encrypted connection', required=False, default='./certificate/nonProd/xyz.pem')
+    return parser.parse_args()
+
+
+def connect_to_dremio_flight_server_endpoint(hostname, flightport, username, password, sqlquery,
+  tls, certs, disableServerVerification):
+    """
+    Connects to Dremio Flight server endpoint with the provided credentials.
+    It also runs the query and retrieves the result set.
+    """
+
+    try:
+        # Default to use an unencrypted TCP connection.
+        scheme = "grpc+tcp"
+        connection_args = {}
+
+        if tls:
+            # Connect to the server endpoint with an encrypted TLS connection.
+            print('[INFO] Enabling TLS connection')
+            scheme = "grpc+tls"
+            if certs:
+                print('[INFO] Trusted certificates provided')
+                # TLS certificates are provided in a list of connection arguments.
+                with open(certs, "rb") as root_certs:
+                    connection_args["tls_root_certs"] = root_certs.read()
+            elif disableServerVerification:
+                # Connect to the server endpoint with server verification disabled.
+                print('[INFO] Disable TLS server verification.')
+                connection_args['disable_server_verification'] = disableServerVerification
+            else:
+                print('[ERROR] Trusted certificates must be provided to establish a TLS connection')
+                sys.exit()
+
+        # Two WLM settings can be provided upon initial authneitcation
+        # with the Dremio Server Flight Endpoint:
+        # - routing-tag
+        # - routing queue
+        initial_options = flight.FlightCallOptions(headers=[
+            (b'routing-tag', b'test-routing-tag'),
+            (b'routing-queue', b'Low Cost User Queries')
+        ])
+        client_auth_middleware = DremioClientAuthMiddlewareFactory()
+        client = flight.FlightClient("{}://{}:{}".format(scheme, hostname, flightport),
+          middleware=[client_auth_middleware], **connection_args)
+
+        # Authenticate with the server endpoint.
+        bearer_token = client.authenticate_basic_token(username, password, initial_options)
+        print('[INFO] Authentication was successful')
+
+        if sqlquery:
+            # Construct FlightDescriptor for the query result set.
+            flight_desc = flight.FlightDescriptor.for_command(sqlquery)
+            print('[INFO] Query: ', sqlquery)
+
+            # In addition to the bearer token, a query context can also
+            # be provided as an entry of FlightCallOptions.
+            # options = flight.FlightCallOptions(headers=[
+            #     bearer_token,
+            #     (b'schema', b'test.schema')
+            # ])
+
+            # Retrieve the schema of the result set.
+            options = flight.FlightCallOptions(headers=[bearer_token])
+            schema = client.get_schema(flight_desc, options)
+            print('[INFO] GetSchema was successful')
+            print('[INFO] Schema: ', schema)
+
+            # Get the FlightInfo message to retrieve the Ticket corresponding
+            # to the query result set.
+            flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(sqlquery),
+                options)
+            print('[INFO] GetFlightInfo was successful')
+            print('[INFO] Ticket: ', flight_info.endpoints[0].ticket)
+
+            # Retrieve the result set as a stream of Arrow record batches.
+            reader = client.do_get(flight_info.endpoints[0].ticket, options)
+            print('[INFO] Reading query results from Dremio')
+            print(reader.read_pandas())
+
+    except Exception as exception:
+        print("[ERROR] Exception: {}".format(repr(exception)))
+        raise
+
+
+if __name__ == "__main__":
+    # Parse the command line arguments.
+    args = parse_arguments()
+    # Connect to Dremio Arrow Flight server endpoint.
+    connect_to_dremio_flight_server_endpoint(args.hostname, args.flightport, args.username,
+      args.password, args.sqlquery, args.tls, args.trustedCertificates, args.disableServerVerification)
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# dremio-python-client
-A simple Python client to run SQL query in Dremio
+# Python Arrow Flight Client Application Example
+![Build Status](https://github.com/dremio-hub/arrow-flight-client-examples/workflows/python-build/badge.svg)
+
+This lightweight Python client application connects to the Dremio Arrow Flight server endpoint. It requires the username and password for authentication. Developers can use admin or regular user credentials for authentication. Any datasets in Dremio that are accessible by the provided Dremio user can be queried. By default, the hostname is `localhost` and the port is `32010`. Developers can change these default settings by providing the hostname and port as arguments when running the client. Moreover, the tls option can be provided to establish an encrypted connection. 
+> Note: Trusted certificates must be provided when the tls option is enabled.
+
+### Instructions on using this Python sample application
+- Install and setup Python3 as `pyarrow` requires Python3
+- This application also requires `pyarrow` and `pandas`. Consider one of the dependency installation methods below. We recommend using `conda` for its ease of use.
+- Install dependencies using `conda`
+  - `conda install -c conda-forge --file requirements.txt`
+- Alternatively, install dependencies using `pip` 
+  - `pip3 install -r requirements.txt`
+- Run the Python sample application:
+  - `python3 ArrowFlightConnector.py -host '<DREMIO_HOST>' -user '<DREMIO_USERNAME>' -pass '<DREMIO_PASSWORD>'`
+
+```
+usage: ArrowFlightConnector.py [-h] [-host HOSTNAME] [-port FLIGHTPORT] -user USERNAME -pass PASSWORD [-query SQLQUERY] [-tls] [-certs TRUSTEDCERTIFICATES]
+
+E.g. - ArrowFlightConnector.py -user svc_npthcsgdrmro -pass ***** -query "SELECT ASSET_TAG FROM sahub.asset_master_table fetch first 10 rows only" -tls
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -host HOSTNAME, --hostname HOSTNAME Dremio co-ordinator hostname
+  -port FLIGHTPORT, --flightPort FLIGHTPORT Dremio flight server port
+  -user USERNAME, --username USERNAME Dremio username
+  -pass PASSWORD, --password PASSWORD Dremio password
+  -query SQLQUERY, --sqlquery SQLQUERY SQL query to test
+  -tls, --tls Enable encrypted connection
+  -certs TRUSTEDCERTIFICATES, --trustedCertificates TRUSTEDCERTIFICATES Path to trusted certificates for encrypted connection
+```
